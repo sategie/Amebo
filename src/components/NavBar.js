d8de09a23eab67bdea0b1b9e53a1d47bdb8b09ab
sategie <>
2024-01-16 22:17:20 +0000

    Remove container in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,12 @@ import styles from "../styles/NavBar.module.css";
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-      <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="100" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="mx-auto">
             <Nav.Link>
             <i class="fa-solid fa-house"></i>Home
             </Nav.Link>
@@ -27,7 +26,6 @@ const NavBar = () => {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
   );    
 };
