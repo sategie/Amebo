5f74a151d0a6f958321fcecdf6bbbd14de8c617f
sategie <>
2024-01-19 07:05:07 +0000

    Set up login form page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
             <Switch>
             <Route exact path="/" render={() => <h1>Home</h1>} />
             <Route exact path="/posts" render={() => <h1>Posts</h1>} />
-            <Route exact path="/login" render={() => <h1>Login</h1>} />
+            <Route exact path="/login" render={() => <SignInForm />} />
             <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route render={() => <p>Invalid Page!</p>} />
             </Switch>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,97 @@
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import { Form, Button, Image, Col, Row, Container, Alert, } from "react-bootstrap";
+import axios from "axios";
+
+const SignInForm = () => {
+    const [signInInfo, setSignInInfo] = useState({
+        username: "",
+        password:"",
+    });
+    // Destructuring used
+    const {username, password} = signInInfo;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignInInfo({
+            ...signInInfo,
+            // Use Computed Property Names to create a key value pair that can be reused throughout the form
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signInInfo);
+            history.push("/");
+        } catch(err) {
+            // Use optional chaining to handle error display
+            setErrors(err.response?.data);
+        }
+    };
+
+
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>Login</h1>
+
+<Form onSubmit={handleSubmit}>
+  <Form.Group controlId="username">
+    <Form.Label className="d-none">username</Form.Label>
+    <Form.Control className={styles.Input} type="text" placeholder="username" name="username" value={username} onChange={handleChange} />
+  </Form.Group>
+  {errors.username?.map((message,index) => 
+    <Alert variant="warning" key={index} className={styles.Alert}>{message}</Alert>
+  )}
+
+  <Form.Group controlId="password">
+    <Form.Label className="d-none">password</Form.Label>
+    <Form.Control className={styles.Input} type="password" placeholder="password" name="password" value={password} onChange={handleChange} />
+  </Form.Group>
+  {errors.password?.map((message,index) => 
+    <Alert variant="warning" key={index} className={styles.Alert}>{message}</Alert>
+  )}
+  
+  <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+    Login
+  </Button>
+  {errors.non_field_errors?.map((message,index) => 
+    <Alert variant="warning" key={index} className={styles.Alert}>{message}</Alert>
+  )}
+</Form>
+
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Signup</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={
+            "https://res.cloudinary.com/dvfxz4as6/image/upload/v1705523404/nature_gxbcdr.jpg"
+          }
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;
