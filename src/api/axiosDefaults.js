6a0366995c0041e5b485794a7f523f3b01d9d847
sategie <>
2024-01-20 13:04:05 +0000

    Set up axios interceptors to handle access token refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://amebo-drf-18f15fb7ee85.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/contexts/ActiveUserContext.js
+++ src/contexts/ActiveUserContext.js
@@ -1,6 +1,7 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios"
-
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { useHistory } from "react-router";
 export const ActiveUserContext = createContext();
 export const SetActiveUserContext = createContext();
 
@@ -9,6 +10,7 @@ export const useSetActiveUser = () => useContext(SetActiveUserContext);
 
 export const ActiveUserProvider = ({children}) => {
     const [activeUser, setActiveUser] = useState(null);
+    const history = useHistory();
 
     const handleMount = async () => {
         try {
@@ -22,6 +24,48 @@ export const ActiveUserProvider = ({children}) => {
       useEffect(() => {
         handleMount();
       }, []);
+
+      useMemo(() => {
+        axiosReq.interceptors.request.use(
+          async (config) => {
+            try {
+              await axios.post("/dj-rest-auth/token/refresh/");
+            } catch (err) {
+              setActiveUser((prevActiveUser) => {
+                if (prevActiveUser) {
+                  history.push("/login");
+                }
+                return null;
+              });
+              return config;
+            }
+            return config;
+          },
+          (err) => {
+            return Promise.reject(err);
+          }
+        );
+    
+        axiosRes.interceptors.response.use(
+          (response) => response,
+          async (err) => {
+            if (err.response?.status === 401) {
+              try {
+                await axios.post("/dj-rest-auth/token/refresh/");
+              } catch (err) {
+                setActiveUser((prevActiveUser) => {
+                  if (prevActiveUser) {
+                    history.push("/login");
+                  }
+                  return null;
+                });
+              }
+              return axios(err.config);
+            }
+            return Promise.reject(err);
+          }
+        );
+      }, [history]);
 
       return(
         <ActiveUserContext.Provider value={activeUser}>
