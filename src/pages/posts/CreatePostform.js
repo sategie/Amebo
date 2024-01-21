390023445273791b87891353b5b80e33cc2c8ac2
sategie <>
2024-01-21 16:14:24 +0000

    Add error handling to post form

--- src/pages/posts/CreatePostform.js
+++ src/pages/posts/CreatePostform.js
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-
-import Upload from "../../assets/upload.png";
-
+import Alert from "react-bootstrap/Alert";
 import styles from "../../styles/CreateEditPostForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function CreatePostForm() {
     const [errors, setErrors] = useState({});
     
     const[postInfo, setPostInfo] = useState({
         title:"",
-        description:"",
+        post_content:"",
         image:"",
     });
 
-    const { title, description, image } = postInfo;
+    const { title, post_content, image } = postInfo;
+
+    const imageInput = useRef(null)
+
+    const history = useHistory(null)
 
     const handleChange = (event) => {
         setPostInfo({
@@ -40,11 +44,34 @@ function CreatePostForm() {
         };
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+
+        formData.append("title", title);
+        formData.append("post_content", post_content);
+        
+        if(imageInput.current && imageInput.current.files[0]) {
+            formData.append("image", imageInput.current.files[0]);
+        };
+
+        try {
+            const {data} = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch(err) {
+            console.log(err);
+            if (err.response?.status !==401) {
+                setErrors(err.response?.data);
+            }
+
+        }
+    };
+
 
 
 
     return (
-      <Form className={styles.Input}>
+      <Form className={styles.Input} onSubmit={handleSubmit}>
         <Row>
           <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
             <Container
@@ -54,29 +81,34 @@ function CreatePostForm() {
                 <Form.Label>Title</Form.Label>
                 <Form.Control type="text" placeholder="Enter post title" className = {styles.PlaceholderFontSize} name="title" value={title} onChange={handleChange} />
               </Form.Group>
+              {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
               <Form.Group controlId="formBasicPassword" >
                 <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Enter post description" className = {styles.PlaceholderFontSize} name="description" value={description} onChange={handleChange} />
+                <Form.Control as="textarea" rows={3} placeholder="Enter post description" className = {styles.PlaceholderFontSize} name="post_content" value={post_content} onChange={handleChange} />
               </Form.Group>
+              {errors?.post_content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
               <Form.Group >
                 <Form.File
                   id="image-upload"
                   label="Upload Image"
                   accept="image/*"
                   onChange={handleImageChange}
+                  ref={imageInput}
                 />
               </Form.Group>
-              <div className="text-center d-md-none">
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                  onClick={() => {}}
-                >
-                  Cancel
-                </Button>
-                <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-                  Create
-                </Button>
-              </div>
+              {errors?.image?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
             </Container>
           </Col>
           <Col md={5} lg={4} className="p-0 p-md-2 d-none d-md-block">
@@ -85,7 +117,7 @@ function CreatePostForm() {
             >
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Blue} ${styles.LargeButton}`}
-                onClick={() => {}}
+                onClick={() => history.goBack()}
               >
                 Cancel
               </Button>
