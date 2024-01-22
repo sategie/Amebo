f68f9745c836c435eeaaa989f1d7db2317c281bd
sategie <>
2024-01-22 09:34:40 +0000

    Adjust image position and insert border lines

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -8,17 +8,14 @@ import styles from "../../styles/Post.module.css";
 const Post = (props) => {
     const {
         id,
-        own_post,
         profile_id,
         profile_image,
         comments_count,
         likes_count,
         like_id,
-        follower_count,
         title,
         post_content,
         image,
-        created_date,
         updated_date,
         user,
         postPage,
@@ -27,64 +24,66 @@ const Post = (props) => {
 
     const activeUser = useActiveUser();
 
-    const is_own_post = activeUser?.username===own_post;
-
+    const is_own_post = activeUser?.username === user;
 
   return (
     <Card className={styles.Post}>
         <Card.Body>
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {user}
-          </Link>
-          <div className="d-flex align-items-center">
-            <span>{updated_date}</span>
-            {is_own_post && postPage && "..."}
-          </div>
-        </Media>
+            <Media className="align-items-center justify-content-between">
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} height={45} />
+                    {user}
+                </Link>
+                <div className="d-flex align-items-center">
+                    <span>{updated_date}</span>
+                    {is_own_post && postPage && "..."}
+                </div>
+            </Media>
         </Card.Body>
-        <Link to={`/posts/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
 
-      <Card.Body>
-        {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {post_content && <Card.Text>{post_content}</Card.Text>}
-        <div className={styles.PostBar}>
-          {is_own_post ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Sorry, you cannot like your own post!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={() => {}}>
-              <i className={`fas fa-heart ${styles.Heart}`} />
-            </span>
-          ) : activeUser ? (
-            <span onClick={() => {}}>
-              <i className={`far fa-heart ${styles.HeartOutline}`} />
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You need to log in to like posts!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          )}
-          {likes_count}
-          <Link to={`/posts/${id}`}>
-            <i className="far fa-comments" />
-          </Link>
-          {comments_count}
-        </div>
-      </Card.Body>
+        {/* Title moved to be above image and post_content */}
+        {title && <Card.Title className={styles.LargeTitle}>{title}</Card.Title>}
+        <Card.Body>
+            {post_content && <Card.Text>{post_content}</Card.Text>}
+            <hr className={styles.ContentBorder} />
+            <Link to={`/posts/${id}`}>
+                <Card.Img src={image} alt={title} />
+            </Link>
+        </Card.Body>
+        <hr className={styles.ContentBorder} />
 
-
-
+        <Card.Body>
+            <div className={styles.PostBar}>
+                {is_own_post ? (
+                    <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>Sorry, you cannot like your own post!</Tooltip>}
+                    >
+                        <i className="far fa-heart" />
+                    </OverlayTrigger>
+                ) : like_id ? (
+                    <span onClick={() => {}}>
+                        <i className={`fas fa-heart ${styles.Heart}`} />
+                    </span>
+                ) : activeUser ? (
+                    <span onClick={() => {}}>
+                        <i className={`far fa-heart ${styles.HeartOutline}`} />
+                    </span>
+                ) : (
+                    <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>You need to log in to like posts!</Tooltip>}
+                    >
+                        <i className="far fa-heart" />
+                    </OverlayTrigger>
+                )}
+                {likes_count}
+                <Link to={`/posts/${id}`}>
+                    <i className="far fa-comments" />
+                </Link>
+                {comments_count}
+            </div>
+        </Card.Body>
     </Card>
   )
 }
