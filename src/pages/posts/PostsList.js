7b23d3cb24b5ebf7c2469382059dde1213642dd9
sategie <>
2024-01-24 13:00:00 +0000

    Add search feature to posts

--- src/pages/posts/PostsList.js
+++ src/pages/posts/PostsList.js
@@ -3,9 +3,7 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Spinner from "react-bootstrap/Spinner";
-import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsList.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
@@ -13,12 +11,13 @@ import Post from "./Post";
 function PostsList() {
   const [posts, setPosts] = useState({ results: [] });
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         setLoading(true);
-        const { data } = await axiosReq.get("/posts");
+        const { data } = await axiosReq.get(`/posts/?search=${search}`);
         setPosts((prevState) => ({
           ...prevState,
           results: data.results,
@@ -29,18 +28,34 @@ function PostsList() {
         setLoading(false);
       }
     };
-    fetchPosts();
-  }, []);
+    const delaySearch = setTimeout(() => {
+        fetchPosts();
+      }, 500);
+  
+      return () => {
+        clearTimeout(delaySearch);
+      };
+    }, [search]);
+
+
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <Form className="mb-3">
+          <Form.Group className={styles.SearchBar}>
+            <Form.Control
+              type="text"
+              placeholder="Search posts..."
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+          </Form.Group>
+        </Form>
         {loading ? (
-          <div >
-            <Spinner animation="border" variant="success" role="status">
-              <span className="sr-only">Loading...</span>
-            </Spinner>
-          </div>
+          <Spinner animation="border" variant="success" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
         ) : posts.results.length ? (
           posts.results.map((post) => (
             <Post key={post.id} {...post} setPosts={setPosts} />
