c6003f04a40c93c9649cef061f40f197505de73b
sategie <>
2024-01-25 14:07:55 +0000

    Add form for creating comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,10 +9,18 @@ import appStyles from "../../App.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import Post from "./Post";
+import { useActiveUser } from "../../contexts/ActiveUserContext";
+import CreateCommentForm from "../comments/CreateCommentForm";
 
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
+
+    const activeUser = useActiveUser();
+    const profile_image = activeUser?.profile_image;
+    const [comments, setComments] = useState({
+        results: []
+    });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -36,7 +44,18 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Comments
+        {activeUser ? (
+    <CreateCommentForm
+    profile_id={activeUser.profile_id}
+    profileImage={profile_image}
+    post={id}
+    setPost={setPost}
+    setComments={setComments}
+    />
+    ) : comments.results.length ? (
+    "Comments"
+    ) : null}
+          
         </Container>
       </Col>
     </Row>
