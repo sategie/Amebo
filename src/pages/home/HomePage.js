43eac90c6f11333689820bb5638a898d4905c1f4
sategie <>
2024-01-30 11:34:15 +0000

    Set up homepage for logged-in/non-logged in users

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { useActiveUser } from '../../contexts/ActiveUserContext';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import styles from '../../styles/HomePage.module.css';
+import GreenParrot from "../../assets/green-parrot.jpg";
 
 const HomePage = () => {
   const activeUser = useActiveUser();
 
   return (
     <Card style={{ width: 'max-width' }} >
-      <Card.Img variant="top" className = {styles.HeroImage} src="https://res.cloudinary.com/dvfxz4as6/image/upload/v1706178746/media/images/wp3819922-the-cranberries-wallpapers_xsbimp.jpg" />
+      <Card.Img variant="top" className = {styles.HeroImage} src= {GreenParrot} />
       <Card.Body>
         {activeUser ? (
           <>
@@ -34,13 +35,27 @@ const HomePage = () => {
           </>
         ) : (
           <>
-              <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
+        <Card.Body>
+            <Card.Title className = {styles.HomeWelcome} >Welcome to the Amebo App</Card.Title>
+            <Card.Text>
+              Please use the Signup button above to register.
+            </Card.Text>
+            <Card.Text>
+              As a registered user, you would be able to do the following:
+            </Card.Text>
+        </Card.Body>
+            <ListGroup  className='list-group-flush'>
+              <ListGroupItem className = {styles.HomeBorder}></ListGroupItem>
+              <ListGroupItem className = {styles.HomeBorder}>Create your own posts</ListGroupItem>
+              <ListGroupItem className = {styles.HomeBorder}>Comment on and like existing posts</ListGroupItem>
+              <ListGroupItem className = {styles.HomeBorder}>Follow other users</ListGroupItem>
+              <ListGroupItem className = {styles.HomeBorder}></ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+            <Card.Text>
+              If you are already registered, then please use the Login button above to access your account.
+            </Card.Text>
+        </Card.Body>
           </>
         )}
       </Card.Body>
