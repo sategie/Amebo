2e62b6c0e41861644dd881ec9ca23176d52dce9a
sategie <>
2024-02-04 01:59:09 +0000

    Update Profile Page to allow only own profile view followed users

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,5 +1,135 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
+// import Spinner from "react-bootstrap/Spinner";
+// import { Button, Image } from "react-bootstrap";
+
+// import styles from "../../styles/ProfilePage.module.css";
+// import appStyles from "../../App.module.css";
+
+// import btnStyles from "../../styles/Button.module.css";
+// import { useActiveUser } from "../../contexts/ActiveUserContext";
+// import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
+// import { axiosReq } from "../../api/axiosDefaults";
+// import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
+// import { ProfileEditDropdown } from "../../components/DropdownOption";
+
+
+// function ProfilePage() {
+//   const [hasLoaded, setHasLoaded] = useState(false);
+//   const activeUser = useActiveUser();
+//   const {id} = useParams();
+//   const { setProfileData, handleFollow, handleUnfollow } = useSetProfileData();
+//   const { pageProfile } = useProfileData();
+//   const [profile] = pageProfile.results;
+//   const own_profile = activeUser?.username === profile?.user;
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const [{ data: pageProfile }] = await Promise.all([
+//           axiosReq.get(`/profiles/${id}/`),
+//         ]);
+//         setProfileData((prevState) => ({
+//           ...prevState,
+//           pageProfile: { results: [pageProfile] },
+//         }));
+//         setHasLoaded(true);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchData();
+//   }, [id, setProfileData]);
+
+//   const mainProfile = (
+//     <>
+//     {profile?.own_profile && <ProfileEditDropdown id={profile?.id} />}
+//       <Row noGutters className="px-3 text-center">
+//         <Col lg={3} className="text-lg-left">
+//           <Image
+//             className={styles.ProfileImage}
+//             roundedCircle
+//             src={profile?.image}
+//           />
+//         </Col>
+//         <Col lg={6}>
+//           <h3 className="m-2">{profile?.user}</h3>
+//           <Row className="justify-content-center no-gutters">
+//             <Col xs={3} className="my-2">
+//               <div>{profile?.posts_count}</div>
+//               <div>posts</div>
+//             </Col>
+//             <Col xs={3} className="my-2 mx-5">
+//               <div>{profile?.followers_count}</div>
+//               <div>followers</div>
+//             </Col>
+//             <Col xs={3} className="my-2">
+//             {activeUser ? (
+//   <Link to={`/followed-users`}>
+//     <div>{profile?.following_count}</div>
+//     <div>following</div>
+//   </Link>
+// ) : (
+//   <div className={styles.NoClick}>
+//     <div>{profile?.following_count}</div>
+//     <div>following</div>
+//   </div>
+// )}
+
+//             </Col>
+//           </Row>
+//         </Col>
+//         <Col lg={3} className="text-lg-right">
+//           {activeUser &&
+//             !own_profile &&
+//             (profile?.following_id ? (
+//               <Button
+//                 className={`${btnStyles.Button} ${btnStyles.BlackOutline} `}
+//                 onClick={() => handleUnfollow(profile)}
+//               >
+//                 unfollow
+//               </Button>
+//             ) : (
+//               <Button
+//                 className={`${btnStyles.Button} ${btnStyles.Black}`}
+//                 onClick={() => handleFollow(profile)}
+//               >
+//                 follow
+//               </Button>
+//             ))}
+//         </Col>
+//         {profile?.content && <Col className="p-3">{profile.content}</Col>}
+//       </Row>
+//     </>
+//   );
+
+//   return (
+//     <Row>
+//       <Col className="py-2 p-0 p-lg-2" lg={12}>
+//         <Container className={appStyles.Content}>
+//           {hasLoaded ? (
+//             <>
+//               {mainProfile}
+//             </>
+//           ) : (
+//             <Spinner animation="border" variant="success" role="status">
+//             <span className="sr-only">Loading...</span>
+//           </Spinner>
+//           )}
+//         </Container>
+//       </Col>
+//       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+//       </Col>
+//     </Row>
+//   );
+// }
+
+// export default ProfilePage;
+
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -15,7 +145,6 @@ import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
 import { ProfileEditDropdown } from "../../components/DropdownOption";
-
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -67,18 +196,17 @@ function ProfilePage() {
               <div>followers</div>
             </Col>
             <Col xs={3} className="my-2">
-            {activeUser ? (
-  <Link to={`/followed-users`}>
-    <div>{profile?.following_count}</div>
-    <div>following</div>
-  </Link>
-) : (
-  <div className={styles.NoClick}>
-    <div>{profile?.following_count}</div>
-    <div>following</div>
-  </div>
-)}
-
+            {own_profile ? (
+              <Link to={`/followed-users`}>
+                <div>{profile?.following_count}</div>
+                <div>following</div>
+              </Link>
+            ) : (
+              <div className={styles.NoClick}>
+                <div>{profile?.following_count}</div>
+                <div>following</div>
+              </div>
+            )}
             </Col>
           </Row>
         </Col>
@@ -128,4 +256,3 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
-
