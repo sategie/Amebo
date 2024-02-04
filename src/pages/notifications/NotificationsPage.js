cdfe90e9740a828b7752ee7c6d59568142d31110
sategie <>
2024-02-04 08:38:24 +0000

    Update styling for Notifications page message

--- src/pages/notifications/NotificationsPage.js
+++ src/pages/notifications/NotificationsPage.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
 import styles from "../../styles/NotificationsPage.module.css";
-
 
 const NotificationsPage = () => {
   return (
-    <div className={styles.NotificationMessage} >This Page is under construction. Please check back later</div>
+    <Container className={styles.NotificationMessage}>
+      This Page is under construction. Please check back later
+      <i className= {`fa-solid fa-screwdriver-wrench ${styles.Icon}`}></i>
+    </Container>
   )
 }
 
