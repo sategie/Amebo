c9e714a7c83e525ab03210f73c3592cd60313980
sategie <sategie@yahoo.com>
2024-03-07 08:35:50 +0000

    Add code comments to follows

--- src/pages/follows/FollowedUsers.js
+++ src/pages/follows/FollowedUsers.js
@@ -5,39 +5,52 @@ import { useProfileData, useSetProfileData } from '../../contexts/ProfileDataCon
 import { useActiveUser } from '../../contexts/ActiveUserContext';
 import appStyles from '../../App.module.css';
 
+// FollowedUsers component to display profiles that the active user follows
 const FollowedUsers = () => {
+    // State to manage the list of followed users
     const { followedUsers } = useProfileData();
+    // Update profile data using setState
     const { setProfileData } = useSetProfileData();
+    // State to track the loading process
     const [hasLoaded, setHasLoaded] = useState(false);
 
     const activeUser = useActiveUser();
 
     useEffect(() => {
+        // Filter profiles based on following status
         const filterProfiles = (profiles) => {
             let userProfiles = [];
+            // Loop through each profile
             for (let profile of profiles.results) {
+                // Check if the active user is following the profile
                 if (profile.following_id != null) {
                     userProfiles.push(profile);
                 }
             }
 
+            // Update the context state with the filtered followed users
             setProfileData(prevState => ({
                 ...prevState,
                 followedUsers: { results: userProfiles },
             }));
         };
         
+        // Fetch the list of profiles the active user is following
         async function fetchFollowedUsers() {
             try {
+                // API Request to fetch profiles
                 const {data} = await axiosReq.get(`/profiles/`);
 
+                // Filter the received profiles to get the profiles being followed
                 filterProfiles(data);
                 setHasLoaded(true);
             } catch (err) {
+                // Log errors if the request fails
                 console.log(err);
             }
         }
 
+        // Initialize fetching process and reset loading state
         setHasLoaded(false);
         fetchFollowedUsers();
     }, [activeUser, setProfileData]);
@@ -47,9 +60,11 @@ const FollowedUsers = () => {
           {hasLoaded ? (
               followedUsers?.results?.length ? (
                    followedUsers.results.map(profile => (
+                    // Render each followed user's profile using the Profile component
                       <Profile key={profile.id} profile={profile} />
                   ))
               ) : (
+                // Display message if the active user does not follow anyone
                   <div className={`${appStyles.Content}`}>
                       <p>No users followed.</p>
                   </div>
