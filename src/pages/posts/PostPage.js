330766731a3ae69e25db90f36d4307d6eac02f88
sategie <sategie@yahoo.com>
2024-03-08 09:37:42 +0000

    Add comments to PostPage and PostsList components

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -13,23 +13,30 @@ import { useActiveUser } from "../../contexts/ActiveUserContext";
 import CreateCommentForm from "../comments/CreateCommentForm";
 import Comment from "../comments/Comment";
 
+// PostPage component which handles the display of a single post and its comments
 function PostPage() {
+  // Extract post id from URL parameters
     const { id } = useParams();
+    // State which holds post data
     const [post, setPost] = useState({ results: [] });
 
     const activeUser = useActiveUser();
+    // Get profile image if it exists
     const profile_image = activeUser?.profile_image;
     const [comments, setComments] = useState({
         results: []
     });
 
     useEffect(() => {
+      // Handler to load post and comments
         const handleMount = async () => {
           try {
+            // API request to fetch post and comments data at the same time
             const [{ data: post }, { data: comments }] = await Promise.all([
               axiosReq.get(`/posts/${id}`),
               axiosReq.get(`/comments/?post=${id}`),
             ]);
+            // Update state with the fetched data
             setPost({ results: [post] });
             setComments(comments);
           } catch (err) {
@@ -40,12 +47,13 @@ function PostPage() {
         handleMount();
       }, [id]);
 
-
+// Render post and comment data to the browser
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={12}>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
+          {/* Logic to display the CreateCommentForm component if a user is authenticated */}
         {activeUser ? (
     <CreateCommentForm
     profile_id={activeUser.profile_id}
@@ -57,6 +65,7 @@ function PostPage() {
     ) : comments.results.length ? (
     "Comments"
     ) : null}
+    {/* Map through comments array to display each comment */}
     {comments.results.length ? (
             comments.results.map((comment) => (
               <Comment key={comment.id}
--- src/pages/posts/PostsList.js
+++ src/pages/posts/PostsList.js
@@ -10,33 +10,42 @@ import Post from "./Post";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
+// PostsList component which handles the display of all posts
 function PostsList() {
+  // State to hold posts, control loading state, and search inputs
   const [posts, setPosts] = useState({ results: [] });
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
+    // Fetches posts from the API backend based on the current search term
     const fetchPosts = async () => {
       try {
+        // Show loading sign while fetching posts
         setLoading(true);
         const { data } = await axiosReq.get(`/posts/?search=${search}`);
+        // Update state with the data from the fetched posts
         setPosts(data);
         ;
       } catch (err) {
         console.log(err);
       } finally {
+        // Hide loading sign once the data is fetched
         setLoading(false);
       }
     };
+    // Delay the execution of fetchPosts to limit API calls while entering search input
     const delaySearch = setTimeout(() => {
         fetchPosts();
       }, 1000);
   
+      // Cleanup function to cancel the delay when the component unmounts
       return () => {
         clearTimeout(delaySearch);
       };
     }, [search]);
 
+    // Render posts in the browser
     return (
         <Row className="h-100">
           <Col className="py-2 p-0 p-lg-2" lg={12}>
@@ -56,6 +65,7 @@ function PostsList() {
               </Spinner>
             ) : (
               <>
+              {/* // Render posts or show a message if no posts are available */}
                 {posts.results.length ? (
                   <InfiniteScroll
                     children={posts.results.map((post) => (
