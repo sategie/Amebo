b9f1aa2cf5fe89185f7bbc134050317577120dfb
sategie <sategie@yahoo.com>
2024-03-10 16:17:49 +0000

    Add comments to useOutsideClickToggle hook

--- src/hooks/useOutsideClickToggle.js
+++ src/hooks/useOutsideClickToggle.js
@@ -1,15 +1,24 @@
 import {useEffect, useRef, useState} from 'react'
 
+// Custom hook for toggling expanded state on outside clicks
 const useOutsideClickToggle = () => {
+    // State for checking whether the content is expanded
     const [expanded, setExpanded] = useState(false);
+    // Reference to the element on which we want to detect the outside clicks
     const ref = useRef(null);
+    // Hook to listen for clicks outside the referenced element
     useEffect(() => {
+        // Handler to be called on mouse up event
         const handleOutsideClick = (event) => {
+            // Check if the click occurred outside the ref element
             if (ref.current && !ref.current.contains(event.target)) {
+                // Set expanded state to false if the click is outside
                 setExpanded(false)
             }
         }
+        // Add event listener to document to listen for mouse up events
         document.addEventListener("mouseup", handleOutsideClick)
+        // Cleanup function to remove event listener when component unmounts or when effect re-runs
         return () => {
             document.removeEventListener("mouseup", handleOutsideClick)
         }
