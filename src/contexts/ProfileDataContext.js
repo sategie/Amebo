af706d8a04af190a6e8dc023121cd5fa859f89e3
sategie <sategie@yahoo.com>
2024-03-11 08:33:54 +0000

    Add comments to ProfileDataContext

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -3,25 +3,31 @@ import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { followHelper, unfollowHelper } from "../utils/utils";
 import { useActiveUser } from "./ActiveUserContext";
 
+// Create contexts to hold profile data and a function to update it
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
 
+// Custom hooks for using the created contexts
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
+  // State to hold profile information
   const [profileData, setProfileData] = useState({
     pageProfile: { results: [] },
     followedUsers: { results: [] },
   });
 
+  // Access active user data using the custom hook from ActiveUserContext
   const activeUser = useActiveUser
 
+  // Handler to handle following a profile
   const handleFollow = async(selectedProfile) => {
     try {
         const {data} = await axiosRes.post("/followers/", {
             followed_user: selectedProfile.id,
         });
+        // Update profile data with the new follower information
         setProfileData((prevState) => ({
             ...prevState,
             pageProfile: {
@@ -38,10 +44,13 @@ export const ProfileDataProvider = ({ children }) => {
     }
   }
 
+  // Handler to handle unfollowing a profile
   const handleUnfollow = async (selectedProfile) => {
     try {
+      // Make an API delete request to the backend to remove the follower data
       await axiosRes.delete(`/followers/${selectedProfile.following_id}/`);
 
+      // Update state to show the unfollow action by filtering out the unfollowed profile
       setProfileData((prevState) => ({
         ...prevState,
         pageProfile: {
@@ -62,12 +71,15 @@ export const ProfileDataProvider = ({ children }) => {
   };
 
 
+  // Fetch profile data when the component mounts or the active user changes
   useEffect(() => {
     const handleMount = async () => {
       try {
+        // Fetch profiles, ordered by followers count in descending order
         const { data } = await axiosReq.get(
           "/profiles/?ordering=-followers_count"
         );
+        // Set the fetched profile data as the main profile information in the state
         setProfileData((prevState) => ({
           ...prevState,
           mainProfile: data,
@@ -80,6 +92,7 @@ export const ProfileDataProvider = ({ children }) => {
     handleMount();
   }, [activeUser]);
 
+  // Provide the profile data and functions to update it down the children components
   return (
     <ProfileDataContext.Provider value={profileData}>
       <SetProfileDataContext.Provider
